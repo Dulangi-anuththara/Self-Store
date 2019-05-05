2e2cc120a0c574990f3d7d334580b0e13f3ef0e4
DulangiAnuththara <43141556+DulangiAnuththara@users.noreply.github.com>
2019-05-05 10:39:00 +0530

    fifth test commit

--- PaymentTest.php
+++ PaymentTest.php
@@ -1,6 +1,24 @@
 <?php
 
-echo 'Test 1';
+$servername="localhost";
+$username="root";
+$password="";
+$db="self&store";
+
+$conn=mysqli_connect($servername,$username,$password,$db);
+
+if($conn){
+    echo "Connected to database successfully";
+}
+else{
+    die("Could not connect".mysqli_connect_error());
+}
+
+$CustID = $_POST['custID'];
+$date = $_POST['date'];
+$unit = $_POST['unit'];
+$unitID = $_POST['unitID'];
+
 
 
 ?>
